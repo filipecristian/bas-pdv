490860b69b37a6d3d20d29468eac2128d2bb45ee
filipecristian <filipe.oliveira@picpay.com>
2021-08-27 00:00:03 -0300

    Migration para dados setup

---
+++ migrations/20210826_2142.js
@@ -0,0 +1,9 @@
+db.user.insertMany([{
+    "login": "filipe@email.com",
+    "password": "1234",
+    "name": "Filipe Olivira"
+}, {
+    "login": "user@gmail.com",
+    "password": "root",
+    "name": "Fulano da Silva"
+}]);
